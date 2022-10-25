0abfe562d9523fce5b48bb8214e8c7c085715500
jabu <jabu@gx.ag>
2022-10-25 00:47:08 -0700

    more prelude exports

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,3 +2,4 @@ pub use crate::{
     SkeletonController, SkeletonData, Spine, SpineBone, SpineBundle, SpineEvent, SpineLoader,
     SpineReadyEvent, SpineSync, SpineSyncSystem, SpineSystem, SpineSystemFunctions,
 };
+pub use rusty_spine::{BoneHandle, SlotHandle};
